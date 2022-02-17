dcb959a392fc6e5dfe245d84fcf945ad5a1dc9b5
yujeong-jeon <yujeong.jeon@navercorp.com>
2022-02-17 14:38:03 +0900

    🩹 x-cli

--- packages/x-cli/src/main.ts
+++ packages/x-cli/src/main.ts
@@ -1,7 +1,8 @@
-import { awesomeFn } from "@quramy/x-core";
+import { add, awesomeFn } from "@quramy/x-core";
 
 export async function main() {
   // dependencies across child packages
   const out = await awesomeFn();
+  console.log(add([1,2,3]))
   return out;
 }
